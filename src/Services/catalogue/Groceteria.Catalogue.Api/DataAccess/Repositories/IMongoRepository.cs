13ce1cf3dc23799413f9d4ec518c58b55fff5713
sayan95 <sharthakmallik@gmail.com>
2023-02-26 21:59:37 +0530

    Mongo repository update

--- src/Services/catalogue/Groceteria.Catalogue.Api/DataAccess/Repositories/IMongoRepository.cs
+++ src/Services/catalogue/Groceteria.Catalogue.Api/DataAccess/Repositories/IMongoRepository.cs
@@ -1,7 +1,14 @@
-﻿namespace Groceteria.Catalogue.Api.DataAccess.Repositories
+﻿using System.Linq.Expressions;
+
+namespace Groceteria.Catalogue.Api.DataAccess.Repositories
 {
     public interface IMongoRepository<T>
     {
         Task<IReadOnlyCollection<T>> GetAllAsync(string collectionName);
+        Task<IReadOnlyList<T>> GetListByPredicateAsync(Expression<Func<T, bool>> predicate, string collectionName);
+        Task<T> GetByIdAsync(string id, string collectionName);
+        Task<T> GetByPredicateAsync(Expression<Func<T, bool>> predicate, string collectionName);
+        Task UpsertAsync(T entity, string collectionName);
+        Task DeleteAsync(string id, string collectionName); 
     }
 }
--- src/Services/catalogue/Groceteria.Catalogue.Api/DataAccess/Repositories/MongoRepository.cs
+++ src/Services/catalogue/Groceteria.Catalogue.Api/DataAccess/Repositories/MongoRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Groceteria.Catalogue.Api.Entities;
 using MongoDB.Driver;
+using System.Linq.Expressions;
 
 namespace Groceteria.Catalogue.Api.DataAccess.Repositories
 {
@@ -14,15 +15,61 @@ namespace Groceteria.Catalogue.Api.DataAccess.Repositories
             _database = _context.GetMongoDatabaseInstance();
         }
 
+        public async Task DeleteAsync(string id, string collectionName)
+        {
+            var collection = GetCollection<T>(collectionName);
+            await collection.DeleteOneAsync(id);
+        }
+
         public async Task<IReadOnlyCollection<T>> GetAllAsync(string collectionName)
         {
             var collection = GetCollection<T>(collectionName);
             return await collection.Find(item => true).ToListAsync();
         }
 
+        public async Task<T> GetByIdAsync(string id, string collectionName)
+        {
+            var collection = GetCollection<T>(collectionName);
+            return await collection.Find(x => x.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<T> GetByPredicateAsync(Expression<Func<T, bool>> predicate, string collectionName)
+        {
+            var collection = GetCollection<T>(collectionName);
+            FilterDefinition<T> filter = Builders<T>.Filter.Where(predicate);
+            return await collection.Find(filter).FirstOrDefaultAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> GetListByPredicateAsync(Expression<Func<T, bool>> predicate, string collectionName)
+        {
+            var collection = GetCollection<T>(collectionName);
+            FilterDefinition<T> filter = Builders<T>.Filter.Where(predicate);
+            return await collection.Find(filter).ToListAsync();
+        }
+
+        public async Task UpsertAsync(T entity, string collectionName)
+        {
+            var collection = GetCollection<T>(collectionName);
+            if (!string.IsNullOrEmpty(entity.Id) && await IsDocumentUpdateRequest(entity.Id, collection))
+            {
+                await collection.ReplaceOneAsync(x => x.Id== entity.Id, entity);
+            }
+            else
+            {
+                await collection.InsertOneAsync(entity);
+            }
+        }
+
+        private async Task<bool> IsDocumentUpdateRequest(string id, IMongoCollection<T> collection) 
+        {            
+            var entity = await collection.Find(x => x.Id == id).FirstOrDefaultAsync();
+            if (entity == null) return false;
+            return true;
+        }
+
         private IMongoCollection<T> GetCollection<T>(string collectionName)
         {
             return _database.GetCollection<T>(collectionName);
-        }
+        }     
     }
 }
