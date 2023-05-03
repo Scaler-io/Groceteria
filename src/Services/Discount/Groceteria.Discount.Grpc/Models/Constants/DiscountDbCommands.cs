135c55baf5f4cad1cca986d79b0d4b1cd0439428
sharthak mallik <sharthakmallik@gmail.com>
2023-05-03 21:02:09 +0530

    - Creates business logic basic models

---
+++ src/Services/Discount/Groceteria.Discount.Grpc/Models/Constants/DiscountDbCommands.cs
@@ -0,0 +1,22 @@
+﻿namespace Groceteria.Discount.Grpc.Models.Constants
+{
+    public class DiscountDbCommands
+    {
+        public const string SelectAll = "SELECT * FROM Coupon";
+        public const string SelectByProductId = "SELECT * FROM coupon WHERE productid = @ProductId";
+        public const string SelectByProductName = "SELECT * FROM Coupon WHERE ProductName = @ProductName";
+        public const string Insert = "INSERT INTO Coupon (ProductName, Description, Amount, CreatedAt, UpdatedAt) VALUES (@ProductName, @Description, @Amount, @CreatedAt, @UpdatedAt)";
+        public const string Update = "UPDATE Coupon SET ProductName=@ProductName, Description = @Description, Amount = @Amount, UpdatedAt = @UpdatedAt WHERE Id = @Id";
+        public const string Delete = "DELETE FROM Coupon WHERE Id=@Id";
+
+        // DDL COMMANDS
+        public const string DropIfExist = "DROP TABLE IF EXISTS Coupon";
+        public const string CreateCouponTable = @"CREATE TABLE Coupon(Id SERIAL PRIMARY KEY, 
+                                                                ProductId  VARCHAR(50) NOT NULL,
+                                                                ProductName VARCHAR(24) NOT NULL,
+                                                                Description TEXT,
+                                                                Amount INT,
+                                                                CreatedAt TIMESTAMP,
+                                                                UpdatedAt TIMESTAMP)";
+    }
+}
---
+++ src/Services/Discount/Groceteria.Discount.Grpc/Models/Core/MetaData.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace Groceteria.Discount.Grpc.Models.Core
+{
+    public class MetaData
+    {
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public string CreatedBy { get; set; }
+        public string UpdatedBy { get; set; }
+    }
+}
