d3ad999a75baf9eb3c2f085069158e707ecb384b
sharthak mallik <sharthakmallik@gmail.com>
2023-05-03 21:00:18 +0530

    Creates coupon and base entity

---
+++ src/Services/Discount/Groceteria.Discount.Grpc/Entities/Coupon.cs
@@ -0,0 +1,10 @@
+﻿namespace Groceteria.Discount.Grpc.Entities
+{
+    public class Coupon: BaseEntity
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public int Amount { get; set; }
+    }
+}
