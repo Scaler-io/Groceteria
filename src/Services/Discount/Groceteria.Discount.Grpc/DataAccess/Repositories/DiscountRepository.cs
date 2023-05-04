51605c162b47ecab60003d35c42c773aa05e5948
sharthak mallik <sharthakmallik@gmail.com>
2023-05-04 20:54:21 +0530

    creates discount database repository

--- src/Services/Discount/Groceteria.Discount.Grpc/DataAccess/Repositories/DiscountRepository.cs
+++ src/Services/Discount/Groceteria.Discount.Grpc/DataAccess/Repositories/DiscountRepository.cs
@@ -1,37 +1,92 @@
-﻿using Groceteria.Discount.Grpc.Entities;
+﻿using Dapper;
+using Groceteria.Discount.Grpc.Entities;
+using Groceteria.Discount.Grpc.Models.Constants;
+using Groceteria.Shared.Extensions;
+using Npgsql;
+using ILogger = Serilog.ILogger;
 
 namespace Groceteria.Discount.Grpc.DataAccess.Repositories
 {
     public class DiscountRepository : IDiscountRepository
     {
-        public Task<bool> CreateCoupon(Coupon discountCoupon)
+        private readonly ILogger _logger;
+        private readonly IConfiguration _configuration;
+        private readonly NpgsqlConnection _connection;
+
+        public DiscountRepository(ILogger logger, IConfiguration configuration)
         {
-            throw new NotImplementedException();
+            _logger = logger;
+            _configuration = configuration;
+            _connection = EstablishConnection();
         }
 
-        public Task<bool> DeleteCoupon(int id)
+        public async Task<IEnumerable<Coupon>> GetAllCoupons()
         {
-            throw new NotImplementedException();
+            var command = new CommandDefinition(DiscountDbCommands.SelectAll);
+            var coupons = await _connection.QueryAsync<Coupon>(command);
+            return coupons;
         }
 
-        public Task<IEnumerable<Coupon>> GetAllCoupons()
+        public async Task<Coupon> GetCoupon(int id)
         {
-            throw new NotImplementedException();
+            var command = new CommandDefinition(DiscountDbCommands.SelectByProductId);
+            var coupon = await _connection.QueryFirstOrDefaultAsync<Coupon>(command);
+            return coupon;
         }
 
-        public Task<Coupon> GetCoupon(int id)
+        public async Task<Coupon> GetCouponByProductName(string productName)
         {
-            throw new NotImplementedException();
+            var command = new CommandDefinition(DiscountDbCommands.SelectByProductName);
+            var coupon = await _connection.QueryFirstOrDefaultAsync<Coupon>(command);
+            return coupon;
         }
 
-        public Task<Coupon> GetCouponByProductName(string productName)
+        public async Task<bool> CreateCoupon(Coupon discountCoupon)
         {
-            throw new NotImplementedException();
+            var affected = 0;
+            var command = new CommandDefinition(DiscountDbCommands.Insert, new
+            {
+                ProductName = discountCoupon.ProductName,
+                Description = discountCoupon.Description,
+                Amount = discountCoupon.Amount,
+                CreatedAt = discountCoupon.CreatedAt,
+                UpdatedAt = discountCoupon.UpdatedAt
+            });
+            affected = await _connection.ExecuteAsync(command);
+            return affected > 0;
         }
 
-        public Task<bool> UpdateCoupon(Coupon discountCoupon)
+        public async Task<bool> UpdateCoupon(Coupon discountCoupon)
         {
-            throw new NotImplementedException();
+            var affected = 0;
+            var command = new CommandDefinition(DiscountDbCommands.Update, new
+            {
+                ProductName = discountCoupon.ProductName,
+                Description = discountCoupon.Description,
+                Amount = discountCoupon.Amount,
+                UpdatedAt = discountCoupon.UpdatedAt,
+                Id = discountCoupon.Id
+            });
+            affected = await _connection.ExecuteAsync(command);
+            return affected > 0;
+        }
+
+        public async Task<bool> DeleteCoupon(int id)
+        {
+            var affected = 0;
+            var command = new CommandDefinition(DiscountDbCommands.Delete, new { Id = id });
+            affected = await _connection.ExecuteAsync(command);
+            _logger.Here().MethodExited();
+            return affected > 0;
+        }
+
+        private NpgsqlConnection EstablishConnection()
+        {
+            using var connection = new NpgsqlConnection(
+                    _configuration["DiscountDb:ConnectionString"]
+                );
+            _logger.Here().Information("Connection established to discount db");
+            return connection;
         }
     }
 }
