1da24497cd30a8f5fdddaffa48d27aac00b9b4b6
sharthak mallik <sharthakmallik@gmail.com>
2023-05-23 21:21:12 +0530

    Dtabase method repositories

---
+++ src/Services/SalesOrder/Groceteria.SalesOrder.Application/Contracts/Persistance/IOrderRepository.cs
@@ -0,0 +1,9 @@
+﻿using Groceteria.SalesOrder.Domain.Entities;
+
+namespace Groceteria.SalesOrder.Application.Contracts.Persistance
+{
+    public interface IOrderRepository: IBaseRepository<Order>
+    {
+        Task<IEnumerable<Order>> GetOrdersByUserName(string username);
+    }
+}
