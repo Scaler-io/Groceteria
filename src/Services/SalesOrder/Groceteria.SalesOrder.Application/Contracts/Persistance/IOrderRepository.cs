fe7bf520e20eba3831fd9e88f66d4eee6381a076
sharthak mallik <sharthakmallik@gmail.com>
2023-05-24 21:33:19 +0530

    order list query handler for user

--- src/Services/SalesOrder/Groceteria.SalesOrder.Application/Contracts/Persistance/IOrderRepository.cs
+++ src/Services/SalesOrder/Groceteria.SalesOrder.Application/Contracts/Persistance/IOrderRepository.cs
@@ -1,9 +1,10 @@
 ﻿using Groceteria.SalesOrder.Domain.Entities;
+using Groceteria.Shared.Core;
 
 namespace Groceteria.SalesOrder.Application.Contracts.Persistance
 {
     public interface IOrderRepository: IBaseRepository<Order>
     {
-        Task<IEnumerable<Order>> GetOrdersByUserName(string username);
+        Task<IReadOnlyList<Order>> GetOrdersByUserName(string username, RequestQuery query);
     }
 }
