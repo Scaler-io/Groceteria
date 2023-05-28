fa7d2f5e04e19323e5a73fb628d83a399d66ac95
sharthak mallik <sharthakmallik@gmail.com>
2023-05-29 00:00:57 +0530

    Base repository implementation

--- src/Services/SalesOrder/Groceteria.SalesOrder.Application/Contracts/Persistance/IBaseRepository.cs
+++ src/Services/SalesOrder/Groceteria.SalesOrder.Application/Contracts/Persistance/IBaseRepository.cs
@@ -6,13 +6,13 @@ namespace Groceteria.SalesOrder.Application.Contracts.Persistance
 {
     public interface IBaseRepository<T> where T: EntityBase
     {
-        Task<IReadOnlyList<T>> GetAllAsync(RequestQuery query);
-        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate);
-        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null,
+        Task<IReadOnlyList<T>> GetAllAsync(RequestQuery querySpec);
+        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate, RequestQuery querySpec);
+        Task<IReadOnlyList<T>> GetAsync(RequestQuery querySpec, Expression<Func<T, bool>> predicate,
                                         Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
                                         string includeString = null,
                                         bool disableTracking = true);
-        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null,
+        Task<IReadOnlyList<T>> GetAsync(RequestQuery querySpec, Expression<Func<T, bool>> predicate,
                                         Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
                                         List<Expression<Func<T, object>>> includse = null,
                                         bool disableTracking = true);
