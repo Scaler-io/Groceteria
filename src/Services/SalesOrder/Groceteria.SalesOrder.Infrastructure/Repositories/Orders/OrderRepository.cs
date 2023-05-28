f95bbfcc35653ff0d9e8177fca1f031dbfae3888
sharthak mallik <sharthakmallik@gmail.com>
2023-05-29 00:26:33 +0530

    only order repository implementation

---
+++ src/Services/SalesOrder/Groceteria.SalesOrder.Infrastructure/Repositories/Orders/OrderRepository.cs
@@ -0,0 +1,27 @@
+﻿using Groceteria.SalesOrder.Application.Contracts.Persistance;
+using Groceteria.SalesOrder.Domain.Entities;
+using Groceteria.SalesOrder.Infrastructure.Persistance;
+using Groceteria.Shared.Core;
+using System.Linq.Expressions;
+
+namespace Groceteria.SalesOrder.Infrastructure.Repositories.Orders
+{
+    public class OrderRepository : BaseRepository<Order>, IOrderRepository
+    {
+        public OrderRepository(SalesOrderContext context) 
+            : base(context)
+        {
+        }
+
+        public async Task<IReadOnlyList<Order>> GetOrdersByUserName(string username, RequestQuery query)
+        {
+            var includes = new List<Expression<Func<Order, object>>>()
+            {
+                o => o.BillingAddress,
+                o => o.PaymentDetails
+            };
+            var orderList = await GetAsync(query, o => o.UserName == username, null, includes);
+            return orderList;
+        }
+    }
+}
