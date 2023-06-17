c663c878aae1b25de3ad35e2e21a1b2412d558b3
sharthak mallik <sharthakmallik@gmail.com>
2023-06-18 00:36:07 +0530

    dev partially done

---
+++ src/Services/Notification/Groceteria.NotificationMessgae.Processor/DependencyInjections/DataServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Groceteria.NotificationMessgae.Processor.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Groceteria.NotificationMessgae.Processor.DependencyInjections
+{
+    public static class DataServiceExtensions
+    {
+        public static IServiceCollection AddDataServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<NotificationProcessorContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("NotificationProcessor"));
+            });
+            return services;
+        }
+    }
+}
---
+++ src/Services/Notification/Groceteria.NotificationMessgae.Processor/Extensions/HostExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Groceteria.NotificationMessgae.Processor.DataAccess;
+using Groceteria.Shared.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Groceteria.NotificationMessgae.Processor.Extensions
+{
+    public static class HostExtensions
+    {
+        public static async Task<IHost> MigrateDbAsync(this IHost host)
+        {
+            var dbContext = host.Services.GetRequiredService<NotificationProcessorContext>();
+            var logger = host.Services.GetRequiredService<ILogger>();
+            logger.Here().Information("Migration started");
+            if(dbContext == null)
+            {
+                logger.Here().Error("No db context has been found");
+                return host;
+            }
+
+            await dbContext.Database.MigrateAsync();
+            logger.Here().Information("Migration completed");
+            return host;
+        }
+    }
+}
