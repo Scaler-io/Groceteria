2ca9a46890fc49fda52d6c0f94b16c8966fcf58a
sayan95 <35970692+sayan95@users.noreply.github.com>
2023-06-18 00:49:14 +0530

    dev partially done (#12)

---
+++ src/Services/Notification/Groceteria.NotificationMessgae.Processor/DataAccess/Repositories/NotificationHistory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Groceteria.NotificationMessgae.Processor.DataAccess.Repositories
+{
+    public class NotificationHistory : INotificationRepository
+    {
+        public Task<NotificationHistory> GetNotificationHistory()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Services/Notification/Groceteria.NotificationMessgae.Processor/DependencyInjections/ConfigurationExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Groceteria.NotificationMessgae.Processor.DependencyInjections
+{
+    public static class ConfigurationExtensions
+    {
+        public static IServiceCollection ConfigurationSetting(this IServiceCollection services)
+        {
+            var configuration = new ConfigurationBuilder()
+           .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+           .Build();
+
+            services.AddSingleton<IConfiguration>(configuration);
+            return services;
+        }
+    }
+}
