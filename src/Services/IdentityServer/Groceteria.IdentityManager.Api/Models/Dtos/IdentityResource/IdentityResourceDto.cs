bb7c9fa22e66355a6623179bde6aed7a6f21aa49
sharthak95 <sharthakmallik@gmail.com>
2023-12-12 19:44:03 +0530

    IdentityResource dto and summary contract and auto mapping

--- src/Services/IdentityServer/Groceteria.IdentityManager.Api/Models/Dtos/IdentityResource/IdentityResourceDto.cs
+++ src/Services/IdentityServer/Groceteria.IdentityManager.Api/Models/Dtos/IdentityResource/IdentityResourceDto.cs
@@ -2,5 +2,12 @@ namespace Groceteria.IdentityManager.Api.Models.Dtos.IdentityResource;
 
 public class IdentityResourceDto
 {
-    
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string DisplayName { get; set; }
+    public string Description { get; set; }
+    public bool Required { get; set; }
+    public bool Enabled { get; set; }
+    public List<ClaimsDto> UserClaims { get; set; }
+    public MetaDataDto MetaData { get; set; }
 }
